b64d1383a4a7372023a08ed5d9988cd71a29bb27
Karan <kp109882@gmail.com>
2024-03-28 22:24:03 +0530

    initial commit

---
+++ lib/blokChar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/src/material/colors.dart';
+
+class BlokChar {
+  String? text;
+  String? correctText;
+  int? index;
+  bool isFocus = false;
+  bool isCorrect;
+  bool isDefault;
+  bool isExist = false;
+
+  BlokChar(
+    this.text, {
+    this.index,
+    this.isDefault = false,
+    this.correctText,
+    this.isCorrect = false,
+  });
+
+  // declare method used
+
+  get isCorrectPos => correctText == text;
+  setText(String text) {
+    this.text = text;
+    isCorrect = isCorrectPos;
+  }
+
+  setEmpty() {
+    text = "";
+    isCorrect = false;
+  }
+
+  void setColor(MaterialColor green) {}
+}
